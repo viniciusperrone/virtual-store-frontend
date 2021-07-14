420be133ebceedf2465318d1e7ab59022048998f
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-07-14 18:48:09 -0400

    added header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BackgroundComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
